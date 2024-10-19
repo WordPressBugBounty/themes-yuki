57ed0371be8f56a9601f8a7abb865111790f141c
Sean Murphy <sean@iamseanmurphy.com>
2024-10-19 01:16:29 +0000

    1.3.23

---
+++ yuki/lotta-framework/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ibllex/lotta-framework',
+        'pretty_version' => 'v2.0.19',
+        'version' => '2.0.19.0',
+        'reference' => 'fcf5c9bf85f46b12905b0515f36363bea07a5bd6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ibllex/lotta-framework' => array(
+            'pretty_version' => 'v2.0.19',
+            'version' => '2.0.19.0',
+            'reference' => 'fcf5c9bf85f46b12905b0515f36363bea07a5bd6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'v1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => '780e564bf0554c419ef447c2b5b768b497ecee40',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
